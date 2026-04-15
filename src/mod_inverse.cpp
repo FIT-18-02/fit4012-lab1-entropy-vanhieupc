e6b4a60d117368ef6e8d66bfdd2b52ff1f1f0982
vanhieupc <ngovanhieu2006pc@gmail.com>
2026-04-15 14:07:13 +0700

    Update mod_inverse.cpp

--- src/mod_inverse.cpp
+++ src/mod_inverse.cpp
@@ -1,13 +1,7 @@
 #include <iostream>
 using namespace std;
 
-int gcd(int a, int b) {
-    if (b == 0)
-        return a;
-    return gcd(b, a % b);
-}
-
-int extended_euclid(int a, int b, int &x, int &y) {
+int extended_gcd(int a, int b, int &x, int &y) {
     if (b == 0) {
         x = 1;
         y = 0;
@@ -15,7 +9,7 @@ int extended_euclid(int a, int b, int &x, int &y) {
     }
 
     int x1, y1;
-    int gcd = extended_euclid(b, a % b, x1, y1);
+    int gcd = extended_gcd(b, a % b, x1, y1);
 
     x = y1;
     y = x1 - (a / b) * y1;
@@ -25,11 +19,9 @@ int extended_euclid(int a, int b, int &x, int &y) {
 
 int mod_inverse(int a, int m) {
     int x, y;
-    int g = extended_euclid(a, m, x, y);
+    int g = extended_gcd(a, m, x, y);
 
-    if (g != 1) {
-        return -1; // không tồn tại
-    }
+    if (g != 1) return -1;
 
     return (x % m + m) % m;
 }
@@ -42,9 +34,9 @@ int main() {
     int result = mod_inverse(a, m);
 
     if (result == -1)
-        cout << "Khong ton tai nghich dao modulo" << endl;
+        cout << "Khong ton tai nghich dao" << endl;
     else
-        cout << "Modulo inverse: " << result << endl;
+        cout << "Nghich dao: " << result << endl;
 
     return 0;
 }
